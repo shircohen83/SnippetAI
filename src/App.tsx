cb44c273bceb04f292920e3daaa2d1ae1a2272cd
shir.cohen <shir.cohen83@gmail.com>
2025-10-30 21:13:13 +0200

    render the list inode app and delete snippetList

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,31 @@
 import { SnippetEditor } from "./components/snippetEditor"
-import { SnippetList } from "./components/snippetList"
 import { SnippetActions } from "./components/snippetActions"
 import { getSnippets } from "./utils/storage"
 
 export default function App() {
-  const snippets= getSnippets();
+  const snippets = getSnippets()
+
   return (
     <div style={{ padding: 20 }}>
       <h1>SnippetAI</h1>
       <SnippetEditor />
-      <SnippetList />
+
       {snippets.map((s) => (
-        <SnippetActions key={s.id} snippet={s} />
+        <div
+          key={s.id}
+          style={{
+            border: "1px solid #ccc",
+            margin: "12px 0",
+            padding: "12px",
+            borderRadius: "6px",
+          }}
+        >
+          <pre>{s.code}</pre>
+          <small>
+            {s.language} | Tags: {s.tags.join(", ")}
+          </small>
+          <SnippetActions snippet={s} />
+        </div>
       ))}
     </div>
   )
