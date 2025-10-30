a34c91675d5fbae1f54acdd4ae7c519b7f03d95c
shir.cohen <shir.cohen83@gmail.com>
2025-10-30 22:25:40 +0200

    create a seperate css file to design app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,20 @@
 import { SnippetEditor } from "./components/snippetEditor"
 import { SnippetActions } from "./components/snippetActions"
 import { getSnippets } from "./utils/storage"
+import './App.css'
 
 export default function App() {
   const snippets = getSnippets()
 
   return (
-    <div style={{ padding: 20 }}>
-      <h1>SnippetAI</h1>
+    <div className="app-container">
+      <h1 className="title-container">SnippetAI</h1>
       <SnippetEditor />
-
       {snippets.map((s) => (
-        <div
-          key={s.id}
-          style={{
-            border: "1px solid #ccc",
-            margin: "12px 0",
-            padding: "12px",
-            borderRadius: "6px",
-          }}
-        >
-          {/*pre display the text exactly as you typed it in your code. */}
-          <pre>{s.code}</pre> 
-          <span>
+        <div key={s.id} className="snippet-card">
+        {/*pre tag helps to display the text exactly as you typed it in your code. */}
+          <pre className="snippet-code">{s.code}</pre> 
+          <span className="snippet-data">
             {s.language} | Tags: {s.tags.join(", ")}
           </span>
           <SnippetActions snippet={s} />
